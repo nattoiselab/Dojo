34df9a50fd346d948d640743ee42afa661808153
Nattoise Lab <nattoiselab@gmail.com>
2014-08-18 00:24:53 +0800

    Create MemoryObjectStoreFilteringSelectTest1.js

---
+++ dojo/store/MemoryObjectStoreFilteringSelectTest1.js
@@ -0,0 +1,45 @@
+	require(["dojo/dom", 
+	         "dojo/request",
+	         "dojo/_base/window",
+	         "dojo/data/ObjectStore",
+	         "dojo/store/Memory",
+	         "dojo/json",
+	         "dojo/_base/array",
+	         "dijit/form/FilteringSelect",
+	        // "dojo/parser",
+	         "dojo/domReady!"],
+	    function(dom, request, win, ObjectStore, Memory, JSON, arrayUtil, FilteringSelect){
+	        // Results will be displayed in resultDiv
+	        var resultDiv = dom.byId("resultDiv");
+	 
+	        // Request the JSON data from the server
+	        request.get("MemoryObjectStoreFilteringSelectTest1.json", {
+	            // Parse data from JSON to a JavaScript object
+	            handleAs: "json"
+	        }).then(function(data){
+	        	dataStore = new ObjectStore({ objectStore:new Memory({ data: data.items }) });
+	            // Display the data sent from the server
+	           
+	        	var html = "<h2>JSON Data</h2>" +
+	                "<p>JSON encoded data:</p>" +
+	                "<p><code>" + JSON.stringify(data) + "</code></p>";
+	            
+	            resultDiv.innerHTML = html;
+	            
+	            var filteringSelect = new FilteringSelect({
+	                id: "stateSelect",
+	                name: "state",
+	                value: "ENG",
+	                store: dataStore,
+	                searchAttr: "value"
+	            }, "stateSelect");
+	            filteringSelect.placeAt(win.body()).startup();
+	           
+	        },
+	        function(error){
+	            // Display the error returned
+	            resultDiv.innerHTML = error;
+	        });
+	        
+	    }
+	);
