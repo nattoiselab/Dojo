e6c787f38bf0c8a2a79ee820b3c27f0e626fd52a
Nattoise Lab <nattoiselab@gmail.com>
2014-08-17 23:26:12 +0800

    Create ObjectStoreTest1.js

---
+++ Dojo/store/ObjectStoreTest1.js
@@ -0,0 +1,30 @@
+	require(["dojo/dom",
+	         "dojo/request",
+	         "dojo/data/ObjectStore",
+	         "dojo/store/Memory",
+	         "dojo/domReady!"],
+	    function(dom, request, ObjectStore, Memory){
+	        // Results will be displayed in resultDiv
+	        var resultDiv = dom.byId("resultDiv");
+	 
+	        // Request the JSON data from the server
+	        request.get("ObjectStoreTest1.json", {
+	            // Parse data from JSON to a JavaScript object
+	            handleAs: "json"
+	        }).then(function(data){
+	        	dataStore = new ObjectStore({ objectStore:new Memory({ data: data.items }) });
+	            // Display the data sent from the server
+	           
+	        	var html = "<h2>JSON Data</h2>" +
+	                "<p>JSON encoded data:</p>" +
+	                "<p><code>" + JSON.stringify(data) + "</code></p>";
+	            
+	            resultDiv.innerHTML = html;
+	        },
+	        function(error){
+	            // Display the error returned
+	            resultDiv.innerHTML = error;
+	        });
+	        
+	    }
+	);
