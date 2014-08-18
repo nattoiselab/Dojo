6d8b6878a2bc1d908835563189e14b52baeec438
Nattoise Lab <nattoiselab@gmail.com>
2014-08-18 17:03:27 +0800

    Create dropdownButtonTest1.js

---
+++ dijit/form/DropDownButton/dropdownButtonTest1.js
@@ -0,0 +1,7 @@
+require(["dojo/parser", 
+         "dijit/form/ToggleButton", 
+         "dijit/form/ComboButton", 
+         "dijit/Menu", 
+         "dijit/MenuItem", 
+         "dijit/form/DropDownButton", 
+         "dijit/TooltipDialog"]);
