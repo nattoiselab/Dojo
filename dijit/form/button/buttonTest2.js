2b7b5d83d48ca10730c00fdc3acbda42650fc06b
Nattoise Lab <nattoiselab@gmail.com>
2014-08-18 11:51:53 +0800

    Update buttonTest2.js

--- dijit/form/button/buttonTest2.js
+++ dijit/form/button/buttonTest2.js
@@ -5,11 +5,18 @@ require(["dijit/form/Button",
     
 	// Create a button programmatically:
     var myButton = new Button({
-        label: "Click me!",
+        label: "Click me 1!",
         onClick: function(){
             // Do something:
             dom.byId("result1").innerHTML += "Thank you! ";
             console.log("Create a simple button programmatically");
         }
     }, "progButtonNode").startup();
+    
+    var button = new Button({
+        label: "Click Me 2!",
+        onClick: function(){ console.log("First button was clicked!"); }
+    }, "btn");
+    button.startup();
+
 });
