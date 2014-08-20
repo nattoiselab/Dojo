e71ba971996a7eb9db653a6f82cc4277a7aaffa9
Nattoise Lab <nattoiselab@gmail.com>
2014-08-20 17:25:03 +0800

    Create 04TestPage1.js

---
+++ DojoTutorial/04-XHR/04TestPage1.js
@@ -0,0 +1,14 @@
+require(["dojo/query", 
+         "dojo/request/xhr",
+         "dojo/dom-attr",
+         "dojo/NodeList-dom",
+         "dojo/domReady!"
+         ], 
+function(query,
+		 xhr,
+		 domAttr
+){ 
+  //...
+	
+
+});
