568a7b9b564247fc3e584ec61a106d1c959260b5
Nattoise Lab <nattoiselab@gmail.com>
2014-09-02 23:30:14 +0800

    Create buttonTest2.js

---
+++ DojoTutorial/05-FormWidgets/button/buttonTest2.js
@@ -0,0 +1,23 @@
+require(["dijit/form/Button", 
+         "dojo/dom", 
+         "dojo/domReady!"
+        ], function(Button, 
+        			dom){
+    
+	// Create a button programmatically:
+    var myButton = new Button({
+        label: "Click me 1!",
+        onClick: function(){
+            // Do something:
+            dom.byId("result1").innerHTML += "Thank you! ";
+            console.log("Create a simple button programmatically");
+        }
+    }, "progButtonNode").startup();
+    
+    var button = new Button({
+        label: "Click Me 2!",
+        onClick: function(){ console.log("First button was clicked!"); }
+    }, "btn");
+    button.startup();
+
+});
