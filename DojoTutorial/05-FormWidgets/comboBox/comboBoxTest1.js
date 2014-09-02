1eb2377f8b6d2f5ff57b94962ff799b9b34231e8
Nattoise Lab <nattoiselab@gmail.com>
2014-09-02 23:32:33 +0800

    Create comboBoxTest1.js

---
+++ DojoTutorial/05-FormWidgets/comboBox/comboBoxTest1.js
@@ -0,0 +1,32 @@
+require([
+    "dojo/store/Memory", 
+    "dijit/form/ComboBox", 
+    "dojo/domReady!"
+], function(Memory, 
+			ComboBox){
+    var stateStore = new Memory({
+        data: [
+            {name:"Alabama", id:"AL"},
+            {name:"Alaska", id:"AK"},
+            {name:"American Samoa", id:"AS"},
+            {name:"Arizona", id:"AZ"},
+            {name:"Arkansas", id:"AR"},
+            {name:"Armed Forces Europe", id:"AE"},
+            {name:"Armed Forces Pacific", id:"AP"},
+            {name:"Armed Forces the Americas", id:"AA"},
+            {name:"California", id:"CA"},
+            {name:"Colorado", id:"CO"},
+            {name:"Connecticut", id:"CT"},
+            {name:"Delaware", id:"DE"}
+        ]
+    });
+
+    var comboBox = new ComboBox({
+        id: "stateSelect",
+        name: "state",
+        //value: "California",
+        store: stateStore,
+        searchAttr: "name",
+        placeHolder: "Please select one state"
+    }, "stateSelect").startup();
+});
