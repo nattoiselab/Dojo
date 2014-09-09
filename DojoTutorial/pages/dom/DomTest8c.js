33a613934d073075fd50930e696b3d85b728127b
Nattoise Lab <nattoiselab@gmail.com>
2014-09-09 23:59:31 +0800

    Update DomTest8c.js

--- DojoTutorial/pages/dom/DomTest8c.js
+++ DojoTutorial/pages/dom/DomTest8c.js
@@ -4,9 +4,24 @@ require([ "dojo/dom-style",
           "dojo/domReady!" ],
 		function(style, dom, on) {
 			on(dom.byId("command"), "click", function() {
-				var backgroundColor = style.set("divNodeID", "backgroundColor",
+				var backgroundColor = style.set("divNodeID1", "backgroundColor",
 						"green");
-				dom.byId("output").innerHTML = "divNodeID.backgroundColor: "
+				dom.byId("output1").innerHTML = "divNodeID1.backgroundColor: "
 						+ backgroundColor;
+				
+				var divNodeStyleArr = style.set("divNodeID2", { 
+						backgroundColor: "green",
+					      border: "1px dashed black",
+					      margin: "0.5em",
+					      textAlign: "right"
+					});
+				dom.byId("output2").innerHTML = "divNodeStyleArr.backgroundColor: "
+						+ divNodeStyleArr.backgroundColor;
+				dom.byId("output2").innerHTML += "; <br/>divNodeStyleArr.border: " 
+					    + divNodeStyleArr.border;
+				dom.byId("output2").innerHTML += "; <br/>divNodeStyleArr.margin: " 
+				    + divNodeStyleArr.margin;
+				dom.byId("output2").innerHTML += "; <br/>divNodeStyleArr.textAlign: " 
+				    + divNodeStyleArr.textAlign;
 		});
 });
