b0dd23b2548b99fcfbfe903512a77798a44c2b55
Nattoise Lab <nattoiselab@gmail.com>
2014-09-09 23:30:46 +0800

    Update DomTest8b.js

--- DojoTutorial/pages/dom/DomTest8b.js
+++ DojoTutorial/pages/dom/DomTest8b.js
@@ -4,7 +4,7 @@ require([ "dojo/dom-style",
           "dojo/domReady!" ],
 		function(domStyle, dom, on) {
 			on(dom.byId("command"), "click", function() {
-				var backgroundColor = domStyle.get("example", "backgroundColor");
-			    dom.byId("output").innerHTML = "example.backgroundColor: " + backgroundColor;
+				var backgroundColor = domStyle.get("divNodeID", "backgroundColor");
+			    dom.byId("output").innerHTML = "divNodeID.backgroundColor: " + backgroundColor;
 		});
 });
