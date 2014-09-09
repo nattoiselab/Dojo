a5631b02b40f15334608224d99c4fb41635bde70
Nattoise Lab <nattoiselab@gmail.com>
2014-09-10 00:16:31 +0800

    Create DomTest9a.js

---
+++ DojoTutorial/pages/dom/DomTest9a.js
@@ -0,0 +1,25 @@
+require([ "dojo/dom-attr", 
+          "dojo/dom", 
+          "dojo/domReady!" 
+          ], function(domAttr,
+        		  	  dom) {
+	var output = "";
+	
+	if (domAttr.has("model", "name")) {
+		output += "Node model has attribute name <br/>";
+	} else {
+		output += "Node model does not have attribute name <br/>";
+	}
+	if (domAttr.has("model", "baz")) {
+		output += "Node 'model' has attribute baz <br/>";
+	} else {
+		output += "Node 'model' does not have attribute baz <br/>";
+	}
+	if (domAttr.has("model", "foo")) {
+		output += "Node 'model' has attribute foo <br/>";
+	} else {
+		output += "Node 'model' does not have attribute foo <br/>";
+	}
+	
+	dom.byId("output").innerHTML = output;
+});
