193e789ce59ca5b48e4a6915338a540cd968167c
Nattoise Lab <nattoiselab@gmail.com>
2014-09-16 00:09:05 +0800

    Create QueryTest2.js

---
+++ study/pages/02-DojoQuery/QueryTest2.js
@@ -0,0 +1,42 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" href="QueryTest2.css">
+<title>Dojo Access</title>
+
+
+<script
+	type="text/javascript"
+	data-dojo-config="'parseOnLoad': true"
+	src="../../../node_modules/dojo/dojo.js"        
+></script>
+
+<script
+	type="text/javascript"
+	src="QueryTest2.js"
+></script>
+
+
+</head>
+<body>
+   <h2>DojoQuery (2)</h2>
+	<table>
+		<tr><td>AA</td><td>AB</td><td>AC</td></tr>
+		<tr><td>BA</td><td>BB</td><td>BC</td></tr>
+		<tr><td>CA</td><td>CB</td><td>CC</td></tr>
+	</table>
+
+	<article>
+    	<P class="headline">News 1a</P>
+    	<P> News 1b</P>
+    	<P> News 1c</P>
+	</article>
+
+	<article>
+	    <P class="headline">News 2a</P>
+	    <P> News 2b</P>
+	    <P> News 2c</P>
+	</article>
+
+</body>
+</html>
